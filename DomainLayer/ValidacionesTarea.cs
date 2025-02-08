6de393a0f29ae9a2a26d448d51f3c11dc00a46e0
The-Manuel-G <arielnaranjoi829@gmail.com>
2025-02-07 20:38:45 -0400

    delegados

--- DomainLayer/ValidacionesTarea.cs
+++ DomainLayer/ValidacionesTarea.cs
@@ -5,10 +5,7 @@ namespace DomainLayer
 {
     public static class ValidacionesTarea
     {
-        /// <summary>
-        /// Returns true if the Description is not empty
-        /// and the DueDate is in the future.
-        /// </summary>
+       
         public static bool ValidarDescripcionYFecha(Tareas tarea)
         {
             return !string.IsNullOrEmpty(tarea.Description)
